105b2978d554a0182a43385fda374cebacae7432
John McCambridge <johnmccambridge@Johns-MacBook-Pro.local>
2020-02-21 13:07:57 -0600

    fix error messages, add semantic grid

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
     return (
         // good practice to give the root element the same classname as the component itself, "VideoItem" => video-item
         <div onClick={() => onVideoSelect(video)} className="item video-item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="#" />
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
             <div className="content">
                 <div className="header">
                     {video.snippet.title}
